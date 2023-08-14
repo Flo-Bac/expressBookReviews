ea4ed5239dc743e4c07ce0cfdc7cb72798f6d98b
Flo-Bac <florian-bachmann@bluewin.ch>
2023-08-14 11:50:31 -0400

    Project progress

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -50,10 +50,41 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn];
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization["username"];
+        if(review) {
+            filtered_book["reviews"][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`The review for the book with ISBN ${isbn} has been added / updated`)
+    }
+    else{
+        res.send("unable to find this ISBN");
+    }
     
-  
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const reviewer = req.session.authorization["username"];
+
+    let filtered_book = books[isbn];
+    
+    if (!filtered_book) {
+        return res.send("Unable to find this ISBN");
+    }
+
+    if (filtered_book.reviews && filtered_book.reviews[reviewer]) {
+        delete filtered_book.reviews[reviewer];
+        return res.send("The review was deleted");
+    } else {
+        return res.send("No review found for this user and ISBN");
+    }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
