696d9564a0a67aa90c1946957d793c2f616f267a
Flo-Bac <florian-bachmann@bluewin.ch>
2023-08-18 02:08:31 -0400

    Final changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,29 +22,52 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,0,4));
-});
+public_users.get('/', async function (req, res) {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      res.send(JSON.stringify({ books }, null, 4));
+    } catch (error) {
+      console.error("Error while getting book list:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   let isbn = req.params.isbn;
-  res.send(books[isbn])
+  try {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      res.send(books[isbn]);
+    } catch (error) {
+      console.error("No book was found with this ISBN", error);
+      res.status(500).json({message: "Internal server error"});
+  }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   let author = req.params.author;
   let filtered_books = booksArray.filter((book) => book.author === author);
-  res.send(filtered_books)
-
+  try { 
+      await new Promise(resolve => setTimeout(resolve, 100));
+      res.send(filtered_books);
+    } catch (error) {
+        console.error("No book was found with this author", error);
+        res.status(500).json({messaege: "Internal server error"});
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   let title = req.params.title;
   let books_title = booksArray.filter((book) => book.title === title);
-  res.send(books_title)
+  try {
+      await new Promise (resolve => setTimeout(resolve, 100));
+      res.send(books_title);
+  }   catch (error) {
+      console.error("No book was found with this title", error);
+      res.status(500).json({message: "Internal server error"});
+  }
 });
 
 //  Get book review
